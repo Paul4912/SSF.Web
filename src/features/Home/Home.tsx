6028968491a986c79549916662f282a8b5bdd1a9
paul4912 <paulyang4912@gmail.com>
2021-05-21 19:55:57 +1000

    Add buy button

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -12,6 +12,13 @@ const Home: React.SFC<props> = (props: props) => {
     return (
         <div className='Homepage'>
             {render}
+            <button
+                type="button"
+                onClick={(e) => {
+                e.preventDefault();
+                window.location.href='https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x0b151aa48399e0b38af83686f1b094605f8ed2a5';
+            }}
+            >Buy $SSF</button>
         </div>
     )
 }
