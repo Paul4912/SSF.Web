648cefed6e12397eb186e928cfb5dfa2cd09b9f9
paul4912 <paulyang4912@gmail.com>
2021-05-22 13:35:30 +1000

    Placeholder design

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import FlandersImg from '../../Ned_Flanders.png'
 
 interface props {
     users: any[]
@@ -11,14 +13,15 @@ const Home: React.SFC<props> = (props: props) => {
     }
     return (
         <div className='Homepage'>
-            {render}
-            <button
-                type="button"
-                onClick={(e) => {
-                e.preventDefault();
-                window.location.href='https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x0b151aa48399e0b38af83686f1b094605f8ed2a5';
-            }}
-            >Buy $SSF</button>
+            <h1 className='Heading'>Stupid Sexy Flanders</h1>
+            <div className='FlandersImg'>
+                <img src={FlandersImg}/>
+            </div>
+            <div className='BuySSF'>
+                <Button className="Button" size="large" variant="contained" color="primary" onClick={() => { window.open('https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x0b151aa48399e0b38af83686f1b094605f8ed2a5'); }}>
+                    Buy $SSF
+                </Button>
+            </div>
         </div>
     )
 }
